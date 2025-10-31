d06583bfd546953d3d501cc3bcbee433073ecc1a
Marc <marcestupina@gmail.com>
2025-10-31 12:35:08 +0100

    Fixes typo in error message
    Corrects a typo in the error message within the login authentication process.
    
    Also, reorders imports for consistency.

--- internal/send/loginAuth.go
+++ internal/send/loginAuth.go
@@ -25,7 +25,7 @@ func (a *loginAuth) Next(fromServer []byte, more bool) ([]byte, error) {
 		case "Password:":
 			return []byte(a.password), nil
 		default:
-			return nil, errors.New("Unkown fromServer")
+			return nil, errors.New("unknown fromServer")
 		}
 	}
 	return nil, nil
