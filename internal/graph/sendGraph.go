8f938f46baf4119a2b9f9a0629c5089a8eaa334e
Marc <marcestupina@gmail.com>
2025-11-03 14:57:04 +0100

    use azure graph api library

--- internal/graph/sendGraph.go
+++ internal/graph/sendGraph.go
@@ -1,10 +1,17 @@
 package graph
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log/slog"
 	"net/http"
 
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	azidentity "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/kermeth/emailer/internal/send"
 )
 
@@ -22,6 +29,7 @@ type Config struct {
 	TenantId string `json:"tenantId"`
 	AppId    string `json:"appId"`
 	Secret   string `json:"secret"`
+	Sender   string `json:"sender"`
 }
 
 func Handler(writer http.ResponseWriter, request *http.Request) {
@@ -51,5 +59,97 @@ func decodeGraphRequest(request *http.Request) (*Request, error) {
 }
 
 func (request *Request) sendEmail() error {
+	// Create credential using client secret
+	cred, err := azidentity.NewClientSecretCredential(
+		request.Configuration.TenantId,
+		request.Configuration.AppId,
+		request.Configuration.Secret,
+		nil,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create credential: %w", err)
+	}
+
+	// Get access token for Microsoft Graph
+	token, err := cred.GetToken(context.Background(), policy.TokenRequestOptions{
+		Scopes: []string{"https://graph.microsoft.com/.default"},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to get token: %w", err)
+	}
+
+	// Build the email message
+	message := map[string]interface{}{
+		"message": map[string]interface{}{
+			"subject": request.Subject,
+			"body": map[string]string{
+				"contentType": "HTML",
+				"content":     request.Body,
+			},
+			"toRecipients":  buildRecipients(request.To),
+			"ccRecipients":  buildRecipients(request.Cc),
+			"bccRecipients": buildRecipients(request.Bcc),
+		},
+		"saveToSentItems": true,
+	}
+
+	// Add attachments if present
+	if len(request.Attachments) > 0 {
+		message["message"].(map[string]interface{})["attachments"] = buildAttachments(request.Attachments)
+	}
+
+	// Convert to JSON
+	jsonData, err := json.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("failed to marshal message: %w", err)
+	}
+
+	// Send the email via Graph API
+	userEmail := request.Configuration.Sender
+	url := fmt.Sprintf("https://graph.microsoft.com/v1.0/users/%s/sendMail", userEmail)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+	req.Header.Set("Authorization", "Bearer "+token.Token)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusAccepted && resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("failed to send email: status %d, body: %s", resp.StatusCode, string(body))
+	}
+
 	return nil
+}
+
+func buildRecipients(emails []string) []map[string]interface{} {
+	recipients := make([]map[string]interface{}, 0, len(emails))
+	for _, email := range emails {
+		if email != "" {
+			recipients = append(recipients, map[string]interface{}{
+				"emailAddress": map[string]string{
+					"address": email,
+				},
+			})
+		}
+	}
+	return recipients
+}
+
+func buildAttachments(attachments []send.Attachment) []map[string]interface{} {
+	result := make([]map[string]interface{}, 0, len(attachments))
+	for _, att := range attachments {
+		result = append(result, map[string]interface{}{
+			"@odata.type":  "#microsoft.graph.fileAttachment",
+			"name":         att.Name,
+			"contentBytes": att.Data, // Should be base64 encoded
+		})
+	}
+	return result
 }
